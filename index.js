58200dcc3c71e5ae90e9f63e84088421c83c0077
rouxuanooi <b122310400@student.utem.edu.my>
2025-03-30 18:21:07 +0800

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 import { MongoClient } from 'mongodb';
 
+const drivers = [
+    {
+        name: "John Doe",
+        vehicleType: "Sedan",
+        isAvailable: true,
+        rating: 4.8
+    },
+    {
+        name: "Alice Smith",
+        vehicleType: "SUV",
+        isAvailable: false,
+        rating: 4.5
+    }
+];
+
+console.log("All Drivers:", drivers);
+drivers.forEach(driver => console.log('Driver Names: ', driver.name));
+drivers.push({
+    name: "Jenny",
+    vehicleType: "Hatchback",
+    isAvailable: true,
+    rating: 4.7
+});
+console.log("Total number of drivers after updated: ", drivers.length);
+console.log("Updated Drivers List:", drivers);
+
 async function main() {
     const uri = "mongodb://localhost:27017";
     const client = new MongoClient(uri);
@@ -9,15 +35,31 @@ async function main() {
         console.log("Connected to MongoDB!");
 
         const db = client.db("testDB");
-        const collection = db.collection("users");
+        const driversCollection = db.collection("drivers");
 
-        // Insert a document
-        await collection.insertOne({ name: "ooirouxuan", age: 21 });
-        console.log("Document inserted!");
+        drivers.forEach(async(driver)=>{
+            const result = await driversCollection.insertOne(driver);
+            console.log(`New driver created with result: ${result}`);
+        });
 
-        // Query the document
-        const result = await collection.findOne({ name: "ooirouxuan" });
-        console.log("Query result:", result);
+
+        const updateResult = await db.collection('drivers').updateOne(
+            {name: "John Doe"},
+            {$inc: {rating: 0.1}}
+        );
+        //console.log(`Driver updated with result: ${updateResult}`);
+        console.log(`Driver updated. Matched: ${updateResult.matchedCount}, Modified: ${updateResult.modifiedCount}`);
+
+
+        const availableDrivers = await db.collection('drivers').find({
+            isAvailable: true,
+            rating: {$gte: 4.5}
+        }).toArray();
+        console.log("Available drivers:", availableDrivers);
+
+        const deleteResult = await db.collection('drivers').deleteOne({isAvailable:false});
+        //console.log(`Driver deleted with result: ${deleteResult}`);
+        console.log(`Driver deleted. Deleted Count: ${deleteResult.deletedCount}`);
 
     } catch (err) {
         console.error("Error:", err);
